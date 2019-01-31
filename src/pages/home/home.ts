d74b5bca64c12b58e1a14a77f1f05c31961bfaba
Darya Ulyanova <daryaulyanova@admins-iMac-2.local>
2019-01-31 15:32:27 +0200

    added functionality for output weather data

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,26 +2,30 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { WeatherProvider } from '../../providers/weather/weather';
 
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  public location: {
-    city: string
-  }
+  public currentCity: string;
+  public data: Object;
 
   constructor(public navCtrl: NavController,
               private weatherProvider: WeatherProvider) {
-                this.outputResult();
-              }
-
-  outputResult() {
-    console.log('outputResult___');
-    this.location = {
-      city: 'Lviv'
-    }
-    this.weatherProvider.getWeather(this.location.city).subscribe(el => console.log(el));
   }
 
+  ionViewDidLoad () {
+    this.saveForm();
+  }
+
+  saveForm() {
+    if (this.currentCity) {
+      this.weatherProvider.getWeather(this.currentCity).subscribe(res => {
+        this.data = res;
+        console.log(this.data);
+      });
+    }
+  }
 }
+
--- src/providers/weather/weather.ts
+++ src/providers/weather/weather.ts
@@ -1,4 +1,3 @@
-import { Observable } from 'rxjs/Observable';
 import { map } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
@@ -6,17 +5,13 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class WeatherProvider {
-  forecast: Observable<any>;
   API_KEY = '240f1238d82bd897c0ff0885ffe8b4f1';
 
-  constructor(public http: HttpClient) {
-    console.log('Hello WeatherProvider Provider');
-  }
+  constructor(public http: HttpClient) {}
 
   getWeather(city) {
     const url = `http://api.openweathermap.org/data/2.5/weather?q=${city},ua&appid=${this.API_KEY}`;
-    return this.forecast = this.http.get(url)
-    .pipe(map(r => r));
+    return this.http.get(url).pipe(map(data => data));
   }
 
 }
