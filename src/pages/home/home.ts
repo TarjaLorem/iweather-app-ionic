bd8a93e137dd15ea143a3ad0ebb921c8675498b6
TarjaLorem <dashaorluk@gmail.com>
2019-02-27 00:06:09 +0200

    created observable for weather data

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { WeatherProvider } from '../../providers/weather/weather';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 
 @Component({
@@ -8,12 +10,20 @@ import { WeatherProvider } from '../../providers/weather/weather';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  weatherData$: Observable<any>;
+  data = [];
 
   constructor(public navCtrl: NavController,
               private weatherProvider: WeatherProvider) {}
 
 	ionViewDidLoad() {
-		this.weatherProvider.getWeather();
-	}
+    // this.getWeatherData();
+    this.weatherData$ = this.weatherProvider.getWeatherApi('Lviv').pipe(
+      map(({ main }: any) => main && Object.entries(main))
+    ).subscribe(data => console.log(data));
+  }
 
+  // getWeatherData() {
+  //   this.weatherProvider.getWeatherApi('Lviv').subscribe(data => console.log('DATA___', data));
+  // }
 }
